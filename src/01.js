3c9ccbbb8bd2e8f7e7131bcb94bf3a8512eb8355
Szymon Chudy <szymon.chudy@tsh.io>
2022-12-04 13:43:47 +0100

    Add missing day 1

---
+++ src/01.js
@@ -0,0 +1,17 @@
+import { readFile } from './utils/readLines.js'
+
+const input = readFile('01', '\n\n').map(elf => elf.split('\n').filter(e => e))
+
+const totalCalories = input
+  .map(elf => elf
+    .reduce((acc, val) => acc + +val, 0))
+  .sort((a, b) => b - a)
+
+const part1 = totalCalories[0]
+let part2 = 0
+
+for (let i = 0; i < 3; i++) {
+  part2 += totalCalories[i]
+}
+
+console.log({ part1, part2 })
--- src/utils/readLines.js
+++ src/utils/readLines.js
@@ -1,4 +1,4 @@
 import fs from 'fs'
 
-export const readFile = (name) => fs.readFileSync(`../input/${name}.txt`, 'utf8').split('\n')
+export const readFile = (name, splitRules = '\n') => fs.readFileSync(`../input/${name}.txt`, 'utf8').split(splitRules)
 

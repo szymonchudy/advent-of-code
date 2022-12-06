9016c6a026674c040bb8d03685937c7b0e11d7a3
Szymon Chudy <szymon.chudy@tsh.io>
2022-12-06 07:22:12 +0100

    test: fix typo

--- src/__tests__/06.spec.js
+++ src/__tests__/06.spec.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import { it } from 'mocha';
 import { part1, part2 } from '../06.js';
 
-it('Day 05', () => {
+it('Day 06', () => {
   assert.strictEqual(part1, 1538);
   assert.strictEqual(part2, 2315);
 });

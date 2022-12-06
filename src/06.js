36a531d9d2acded22009a0f62f5534be85a5b90f
Szymon Chudy <szymon.chudy@tsh.io>
2022-12-06 07:14:30 +0100

    Add Day 06

---
+++ src/06.js
@@ -0,0 +1,23 @@
+import readLines from './utils/readLines.js';
+
+const input = readLines('06')[0].trim();
+
+const findGroupOfUniqueCharacters = (groupsSize, str = input) => {
+  for (let i = 0; i < str.length; i += 1) {
+    const group = str.slice(i, i + groupsSize);
+    const uniqueCharacters = new Set(group);
+
+    if (uniqueCharacters.size === groupsSize) {
+      return i + groupsSize;
+    }
+  }
+
+  return -1;
+};
+
+const part1 = findGroupOfUniqueCharacters(4);
+const part2 = findGroupOfUniqueCharacters(14);
+
+console.log('Day 06:', { part1, part2 });
+
+export { part1, part2 };
---
+++ src/__tests__/06.spec.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import { it } from 'mocha';
+import { part1, part2 } from '../06.js';
+
+it('Day 05', () => {
+  assert.strictEqual(part1, 1538);
+  assert.strictEqual(part2, 2315);
+});
